cf7a76e8ee122eb7475207eeeb6a6854c53ee249
Andrew Maxwell <andrewjamax@gmail.com>
2025-10-21 16:41:04 +1100

    Disables texture filtering.

--- ProjectMayhem/DungeonGame.cpp
+++ ProjectMayhem/DungeonGame.cpp
@@ -14,6 +14,7 @@ void DungeonGame::LoadTextures(SDL_Renderer* renderer)
 {
 	// Load all textures
 	this->texHero = IMG_LoadTexture(renderer, path_Hero.c_str());
+	SDL_SetTextureScaleMode(texHero, SDL_SCALEMODE_NEAREST);    // Disables texture filtering for an old-school look.
 
 	this->RectHero = { 0, 0, tileSizeX, tileSizeY };
 
